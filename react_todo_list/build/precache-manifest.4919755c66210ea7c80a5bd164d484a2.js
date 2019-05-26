c1fa109365f5e2c64e055d343991261dd472e52b
NancyAr <39864398+NancyAr@users.noreply.github.com>
2019-05-26 11:57:15 +0200

    Add files via upload

---
+++ react_todo_list/build/precache-manifest.4919755c66210ea7c80a5bd164d484a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdff691af63e6848b3593b657d457da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666b163b326e87c9dc08",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "254a14c09bfef11176c1",
+    "url": "/static/js/2.ccc89068.chunk.js"
+  },
+  {
+    "revision": "666b163b326e87c9dc08",
+    "url": "/static/js/main.d99429e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
